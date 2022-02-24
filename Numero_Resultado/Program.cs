ef0bbbfd2a807388fa818af17b132cc37c2de273
Fabio2222 <fabiomoreira73@outlook.com>
2022-02-24 10:19:13 -0300

    Add project files.

---
+++ Numero_Resultado/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Numero_Resultado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int n1, n2, soma;
+
+            Console.WriteLine("Informe o primeiro numero!");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe o segundo numero!");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            soma = n1 + n2;
+
+            if ((n1 + n2) % 2 == 0)
+            
+                Console.WriteLine("A soma dos numeros digitados e {0} o numero e Par.", soma);            
+            
+            else
+            
+                Console.WriteLine("A soma dos numeros digitados e {0} o numero e IMPAR.", soma);
+            
+        }
+    }
+}
